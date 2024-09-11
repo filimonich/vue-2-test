d3e3919041665275aa569efd7aee94058d3875ec
SergeyF <srfilimonoff@gmail.com>
2024-09-11 08:55:50 +0300

    updated build

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "docs",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-2-test/" : "/",
   css: {
     loaderOptions: {
       sass: {
