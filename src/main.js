497a71febfa1dd7a63c60725da71775112d8bb3d
SergeyF <srfilimonoff@gmail.com>
2024-09-11 09:57:38 +0300

    update build

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./assets/styles/main.scss";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import "@/assets/styles/main.scss";
 
 Vue.config.productionTip = false;
 
